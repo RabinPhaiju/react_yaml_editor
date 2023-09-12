3124ac88bdcc2cc6b05ef92d3cb4caefab315df2
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-09-12 12:24:17 +0545

    added new references.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import yaml from 'yaml';
 import getKeys from "./getKeys";
 
 export default function App() {
-  const [ data, setData ] = useState(`
+  const templateData = `
 - references:
     - name: prashna_kundali
       content:
@@ -55,15 +55,15 @@ export default function App() {
               - text: 'her'
           female:
               - text: 'him'
-`
-  );
+`;
 
+  const [ data, setData ] = useState('')
   const [suggestions, setSuggestions] = useState([]);
   const [dataObject, setDataObject] = useState([]);
 
   useEffect(() => {
     // let value = parser.load(data);
-    let value = parser.loadAll(data)[0];
+    let value = parser.loadAll(templateData)[0];
     let _suggestions = value.flatMap(obj => getKeys(obj));
     setSuggestions(_suggestions);
     
@@ -76,11 +76,13 @@ export default function App() {
 
 
     // split first array
-    let document = yaml.parseDocument(data);
+    let document = yaml.parseDocument(templateData);
     let firstDocument = document.contents.items[0];
     let firstDocumentString = yaml.stringify([firstDocument]);
     // console.log(firstDocumentString);
     setDataObject(firstDocumentString);
+
+    addNewReference(firstDocumentString,setDataObject);
 
     // split except first array
     let exceptfirstDocument = document.contents.items.slice(1);
@@ -90,25 +92,45 @@ export default function App() {
 
   },[])
 
+  const addNewReference = (data,set) => {
+    let value = parser.loadAll(data)[0];    
+    let references = value[0];
+
+    references['references'].push({
+      name: 'new_item',
+      content: [
+         {
+          ext_link: {
+            code: 'new_item',
+            source: 'ext_link'
+          }
+        }
+      ]
+    });
+    references = yaml.stringify([references]);
+    set(references);
+  }
+
   const previewYaml = () => {
-    console.log(data);
+    let mergedYaml = dataObject + '\n' + data;
+    setData(mergedYaml);
   }
 
   return (
     <div className="App">
         <div style={{position:'relative'}} >
         <div style={{position:'sticky', top:'0px',zIndex:'1',backgroundColor:'#fff'}}>
-            <span>References Readonly</span>
+            <span>References</span>
             {/* <YamlEditor data={ 
               (data.length > 0 && dataObject?.length > 0 ) ? parser.dump([dataObject[1]]): ''
               } onChange={setData} previewYaml={previewYaml} readOnly={true} /> */}
 
           <YamlEditor 
           data={ dataObject?.length > 0 ? dataObject : '' } 
-          onChange={()=>{}} previewYaml={previewYaml} suggestions={suggestions} />
+          onChange={setDataObject} previewYaml={previewYaml} suggestions={suggestions} readOnly={true} />
           <span>Templates</span>
         </div>
-          <YamlEditor data={data} onChange={setData} previewYaml={previewYaml} suggestions={suggestions} />
+          <YamlEditor data={data.length > 0 ? data : ''} onChange={setData} previewYaml={previewYaml} suggestions={suggestions} />
         </div>
     </div>
   );
