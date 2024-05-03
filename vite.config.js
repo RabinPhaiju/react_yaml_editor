e0b01b3bb7157ad872cb6bcd95116855ee3b64f6
Rabin Phaiju <rabinphaiju15@gmail.com>
2024-05-03 12:21:41 +0545

    added dist

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
     export default defineConfig({
     hmr: 'localhost',
   plugins: [react()],
+  base: '/react_yaml_editor',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
